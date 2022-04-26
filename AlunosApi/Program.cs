b6f80f277db46b00f55e531acc3a5dd4e4839f1c
Caio Caldas <caiorochacaldas99@gmail.com>
2022-04-26 15:44:28 -0300

    Antes da migration

--- AlunosApi/Program.cs
+++ AlunosApi/Program.cs
@@ -1,3 +1,6 @@
+using AlunosApi.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//coneção sql server default aqui mesmo
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer("Data Source=LAPTOP-6KK5BAFI\\SQLEXPRESS;Initial Catalog=AlunosDemoDB;Integrated Security=True"));
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
@@ -15,6 +23,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+
 
 app.UseHttpsRedirection();
 
