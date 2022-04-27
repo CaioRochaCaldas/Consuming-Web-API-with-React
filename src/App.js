1028f153e7e118ea35c2f369e381648e2aec0353
Caio Caldas <caiorochacaldas99@gmail.com>
2022-04-27 16:10:45 -0300

    inicio implementado

--- src/App.js
+++ src/App.js
@@ -4,10 +4,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 import {Modal,ModalBody,ModalFooter,ModalHeader} from 'reactstrap';
 import logoCadastro from './assets/cadastro.png';
+import { useEffect,useState } from 'react';
 
 function App() {
 
 const baseUrl ="https://localhost:7243/api/alunos";
+
+const [data,setData] = useState([]);
+
+const pedidoGet = async()=>{
+  await axios.get(baseUrl).then(response => { //requisição get ao endereço definido
+    setData(response.data);
+  }).catch(error=>{
+    console.log(error);
+  })
+}
+
+useEffect(()=>{
+  pedidoGet();
+})
+
 
   return (
     <div className="App">
@@ -28,6 +44,19 @@ const baseUrl ="https://localhost:7243/api/alunos";
           </tr>
         </thread>
         <tbody>
+
+          {data.map(aluno=>( //mapeie o usestate dos dados do Get e mostre o id,nome,email,idade lá da api
+            <tr key={aluno.id}>
+              <td>{aluno.id}</td>
+              <td>{aluno.name}</td>
+              <td>{aluno.email}</td>
+              <td>{aluno.idade}</td>
+              <td>
+                <button className='btn btn-primary'>Editar</button>
+                <button className='btn btn-danger'>Excluir</button>
+              </td>
+            </tr>
+          ))}
 
         </tbody>
       </table>
