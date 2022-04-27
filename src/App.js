800237c15af14f46f779abe0265b6f20e8526f1f
Caio Caldas <caiorochacaldas99@gmail.com>
2022-04-27 14:33:52 -0300

    Url e dados html do App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import {Modal,ModalBody,ModalFooter,ModalHeader} from 'reactstrap';
+import logoCadastro from './assets/cadastro.png';
 
 function App() {
+
+const baseUrl ="https://localhost:7243/api/alunos";
+
   return (
     <div className="App">
-      <header className="App-header">
-       
-      </header>
+     <br/>
+     <h3>Cadastro de Alunos</h3>
+     <header>
+       <img src={logoCadastro} alt='Cadastro'/>
+        <button className='btn btn-success'>Incluir Novo Aluno</button>
+     </header>
+      <table className='table table-bordered'>
+        <thread>
+          <tr>
+            <th>Id</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Idade</th>
+            <th>Operação</th>
+          </tr>
+        </thread>
+        <tbody>
+
+        </tbody>
+      </table>
     </div>
   );
 }
